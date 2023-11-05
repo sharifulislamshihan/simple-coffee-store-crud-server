88c8c7c0d45ad5475aaaa908a79b99d695a2f8a8
sharifulislamshihan <sharifulislamshihan@gmail.com>
2023-11-06 02:39:20 +0600

    delete operation completed

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ async function run() {
         app.get('/coffee/:id', async (req, res) => {
             const id = req.params.id;
             const query = {
-                _id : new ObjectId(id)
+                _id: new ObjectId(id)
             }
             const result = await coffeeCollection.findOne(query);
             res.send(result);
@@ -60,6 +60,32 @@ async function run() {
             const newCoffee = req.body;
             console.log(newCoffee);
             const result = await coffeeCollection.insertOne(newCoffee);
+            res.send(result);
+        })
+
+        // put operation for update
+
+        app.put('/coffee/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = {
+                _id: new ObjectId(id)
+            };
+            const options = {
+                upsert: true
+            };
+            const updatedCoffee = req.body;
+            const coffee = {
+                $set: {
+                    name: updatedCoffee.name,
+                    chef: updatedCoffee.chef,
+                    price: updatedCoffee.price,
+                    taste: updatedCoffee.taste,
+                    category: updatedCoffee.category,
+                    details: updatedCoffee.details,
+                    photo: updatedCoffee.photo
+                }
+            }
+            const result = await coffeeCollection.updateOne(filter, coffee, options);
             res.send(result);
         })
 
